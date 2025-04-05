4b5f78f0b26a464e50534ceec539f4b183cdf6b5
Shraaavaaaniii <patilshravani302004@gmail.com>
2025-04-05 10:04:22 +0530

    img

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,53 @@
+import { Button } from "@/components/ui/button";
+import { Container } from "@/components/ui/container";
+import { Sparkles } from "lucide-react";
+
 const HomePage = () => {
-  return <div></div>;
+  return (<div className="flex-col w-full pb-24">
+    <Container>
+      <div className="my-8">
+        <h2 className="text-3xl text-center md:text-left md:text-6xl">
+          <span className="text-outline font-extrabold md:text-8xl">
+          AI Superpower 
+          </span>
+          <span className="text-gray-500 font-extrabold">
+            -A better way to  
+          </span>
+          <br />
+          Improve your interview chances and skills
+        </h2>
+        <p className="mt-4 text-muted-foreground text-sm">
+          Boost your interview skills and increase your success rate with AI driven insights
+        </p>
+
+        </div> 
+        /*image sectiomn */
+
+        <div className="w-full mt-4 rounded-xl bg-gray-100 h-[420px] drop-shadow-md overflow-hidden relative">
+          <img
+            src="/img/hero.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+
+          <div className="absolute top-4 left-4 px-4 py-2 rounded-md bg-white/40 backdrop-blur-md">
+            Inteviews Copilot&copy;
+          </div>
+
+          <div className="hidden md:block absolute w-80 bottom-4 right-4 px-4 py-2 rounded-md bg-white/60 backdrop-blur-md">
+            <h2 className="text-neutral-800 font-semibold">Developer</h2>
+            <p className="text-sm text-neutral-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              distinctio natus, quos voluptatibus magni sapiente.
+            </p>
+
+            <Button className="mt-3">
+              Generate <Sparkles />
+            </Button>
+          </div>
+      </div>  
+    </Container>
+  </div>);
 };
 
 export default HomePage;
